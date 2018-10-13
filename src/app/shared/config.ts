f9e856a8ddbed914616bd55a4d93139747eb640a
Dieu Ngoc Bao <dieungocbao@gmail.com>
2018-10-13 13:13:24 +0700

    change host

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-    public apiHost = "https://tpltesting.herokuapp.com/api";
-    public host = "https://tpltesting.herokuapp.com";
+    public apiHost = "https://dakcnhom17.herokuapp.com/api";
+    public host = "https://dakcnhom17.herokuapp.com";
 }
